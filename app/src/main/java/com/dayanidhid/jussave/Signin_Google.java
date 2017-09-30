627177bc3091c7bc54ddfe7d14d3648629771b6c
GithubHawks <msubash97@gmail.com>
2017-10-01 00:46:06 +0530

    SignInGoogle only UI, without functionality

---
+++ app/src/main/java/com/dayanidhid/jussave/Signin_Google.java
@@ -0,0 +1,19 @@
+package com.dayanidhid.jussave;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by subash on 1/10/17.
+ */
+
+public class Signin_Google extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
