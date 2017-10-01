c8ea186719ecea733f68e09ee64baae1be5e4a79
TechWithFreaks <krishdhamo1997@gmail.com>
2017-10-02 02:09:36 +0530

    Remainder with new UI and delete option

---
+++ app/src/main/java/com/dayanidhid/jussave/Adapters/RemRecyclerView.java
@@ -0,0 +1,71 @@
+package com.dayanidhid.jussave.Adapters;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.dayanidhid.jussave.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by HP on 10/1/2017.
+ */
+
+public class RemRecyclerView extends RecyclerView.Adapter<RemRecyclerView.ViewHolder> {
+    private int listItemLayout5;
+   public ArrayList<RemAdapter> itemList5;
+    public ArrayList<RemAdapter> selectedlist;
+    Context mcontext;
+    public RemRecyclerView(Context context, int listItemLayout, ArrayList<RemAdapter> itemList, ArrayList<RemAdapter> selectedlist1)
+    {
+        this.listItemLayout5=listItemLayout;
+        this.itemList5=itemList;
+        this.selectedlist=selectedlist1;
+        this.mcontext=context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(listItemLayout5, parent, false);
+        final ViewHolder myViewHolder3 = new ViewHolder(view);
+        return myViewHolder3;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position)
+    {
+        if(selectedlist!=null) {
+            if (selectedlist.contains(itemList5.get(position))) {
+                holder.ll.setBackgroundColor(ContextCompat.getColor(mcontext, R.color.list_item_selected_state));
+            } else {
+                holder.ll.setBackgroundColor(ContextCompat.getColor(mcontext, R.color.list_item_normal_state));
+            }
+        }
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return itemList5==null?0:itemList5.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+    {
+        public LinearLayout ll;
+        public ViewHolder(View itemView)
+        {
+            super(itemView);
+            ll=(LinearLayout)itemView.findViewById(R.id.linearLayout);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+}
