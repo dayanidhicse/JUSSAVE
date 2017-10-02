3d872f69027e88b0986b055d447be3f9f784bf08
GithubHawks <msubash97@gmail.com>
2017-10-02 14:52:43 +0530

    File Creation(json) Have to Be done

--- app/src/main/java/com/dayanidhid/jussave/GetInput/InputNote.java
+++ app/src/main/java/com/dayanidhid/jussave/GetInput/InputNote.java
@@ -1,5 +1,7 @@
 package com.dayanidhid.jussave.GetInput;
 
+import android.content.res.AssetManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.Nullable;
@@ -8,8 +10,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.dayanidhid.jussave.R;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -18,10 +24,14 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 /**
@@ -32,9 +42,11 @@ public class InputNote extends AppCompatActivity
 {
     Button savebut;
     EditText about,contenttxt;
-    String Title,Content,Src,CreatedAt,UpdatedAt,isFav;
+    String Title,Data,Src,CreatedAt,UpdatedAt,isFav;
     final static String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Jussave/" ;
-
+    File gpxfile;
+    int count=0;
+    ArrayList<JSONObject> Content = new ArrayList<JSONObject>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -47,15 +59,77 @@ public class InputNote extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                Title = about.getText().toString();
-                Content = contenttxt.getText().toString();
-                Src = "";
-                CreatedAt = new SimpleDateFormat("dd-MM-yyyy").format(Calendar.getInstance().getTime());
-                UpdatedAt = CreatedAt;
-                isFav = "";
-                String line = null;
-
-
+//            try
+//            {
+//                Title = about.getText().toString();
+//                Data = contenttxt.getText().toString();
+//                Src = "";
+//                CreatedAt = new SimpleDateFormat("dd-MM-yyyy").format(Calendar.getInstance().getTime());
+//                UpdatedAt = CreatedAt;
+//                isFav = "";
+//                JSONObject MyNotes  = new JSONObject();
+//                JSONObject obj = new JSONObject();
+//                JSONObject D = new JSONObject();
+////                    JSONObject Content = new JSONObject();
+//                D.put("Title",Title);
+//                D.put("Data",Data);
+//                D.put("Type","txt");
+//                D.put("Src",Src);
+//                D.put("CreatedAt",CreatedAt);
+//                D.put("UpdatedAt",UpdatedAt);
+//                D.put("isFav",isFav);
+//
+//                Content.add(D);
+//                System.out.print(Content);
+//                obj.put("Content",Content);
+//                obj.put("Count",count);
+//                System.out.println(obj);
+//                System.out.println("!@#$%^");
+//
+//                MyNotes.put("Mynotes",obj);
+//                System.out.println(obj);
+//                System.out.println("!@#$%^");
+////                    Toast.makeText(getApplicationContext(), MyNotes, Toast.LENGTH_SHORT).show();
+//
+//
+////                    String line = Title+"#"+Data+"#"+Src+"#"+CreatedAt+"#"+UpdatedAt+"#"+isFav;
+//                File root = new File(Environment.getExternalStorageDirectory(), "Jussave");
+//                if (!root.exists()) {
+//                    root.mkdirs();
+//                    gpxfile = new File(root, "MyNotes.json");
+//                    Toast.makeText(getApplicationContext(), "MKDIR", Toast.LENGTH_SHORT).show();
+//                    gpxfile = new File(root, "MyNotes.txt");
+//                    FileWriter writer = new FileWriter(gpxfile);
+//                    System.out.println("BOWWW "+MyNotes.toString());
+//                    writer.append((CharSequence) MyNotes);
+//                    writer.flush();
+//                    writer.close();
+//                    Toast.makeText(getApplicationContext(), (CharSequence) MyNotes, Toast.LENGTH_SHORT).show();
+//
+//                }
+//                System.out.println("ROOT "+root.exists());
+//                if(root.exists())
+//                {
+//                    File file = new File(path, "MyNotes.json");
+//                    int length = (int) file.length();
+//                    byte[] bytes = new byte[length];
+//                    FileInputStream in = new FileInputStream(file);
+//                    try {
+//                        in.read(bytes);
+//                    } finally {
+//                        in.close();
+//                    }
+//                    String contents = new String(bytes);
+//                    JSONObject jsonObject = new JSONObject(contents);
+//                    System.out.println(" CONTENTS "+contents);
+//                }
+//
+//
+//            }
+//            catch(Exception e)
+//            {
+//                    System.out.println("EXCEPTION "+e);
+//            }
 
             }
         });
